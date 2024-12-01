5d11b5427811fd10df5285987c5ec7e57764d857
richard <rboss@kth.se>
2024-12-01 20:43:58 +0100

    day1 complete

---
+++ 2024/day1.fsx
@@ -0,0 +1,25 @@
+let input =
+    "./day1_input.txt"
+    |> System.IO.File.ReadAllLines
+    |> Array.map (fun line -> line.Split(' ', System.StringSplitOptions.RemoveEmptyEntries))
+    |> Array.map (fun [| l; r |] -> int l, int r)
+    |> Array.unzip
+
+input
+||> (fun l r -> Array.sort l, Array.sort r)
+||> Array.zip
+|> Array.map (fun (l, r) -> System.Math.Abs(l - r))
+|> Array.sum
+|> printfn "Day 1a : %i"
+
+
+input
+||> (fun l r ->
+    let countMap = r |> Array.countBy id |> Map.ofArray
+
+    let getCount v =
+        Map.tryFind v countMap |> Option.defaultValue 0
+
+    l |> Array.map (fun v -> getCount v * v))
+|> Array.sum
+|> printfn "Day 1b : %i"
