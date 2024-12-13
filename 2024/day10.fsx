540aa83e72f67f3f636ae8bea881e4485438ae7c
richard <rboss@kth.se>
2024-12-13 17:22:08 +0100

    day10b

--- 2024/day10.fsx
+++ 2024/day10.fsx
@@ -1,10 +1,10 @@
 let input =
     "./day10_input.txt" |> System.IO.File.ReadAllLines |> Array.map Seq.toArray
 
-let example =
-    "./day10_example.txt" |> System.IO.File.ReadAllLines |> Array.map Seq.toArray
-
 let charToInt = System.Globalization.CharUnicodeInfo.GetDigitValue
+
+let adjacent (y, x) =
+    [ (y + 1, x); (y - 1, x); (y, x + 1); (y, x - 1) ]
 
 let topoMap =
     input
@@ -14,10 +14,7 @@ let topoMap =
 
 let trailheads = topoMap |> Map.filter (fun key v -> v = 0) |> Map.toList
 
-let adjacent (y, x) =
-    [ (y + 1, x); (y - 1, x); (y, x + 1); (y, x - 1) ]
-
-let trailscore (topoMap: Map<(int * int), int>) startPos =
+let trailscore topoMap startPos =
     let findPos pos =
         Map.tryFind pos topoMap |> Option.map (fun v -> pos, v)
 
@@ -26,13 +23,13 @@ let trailscore (topoMap: Map<(int * int), int>) startPos =
         | [] -> acc
         | (pos, height) :: rest when height = 9 -> nextStep rest (pos :: acc)
         | (pos, height) :: rest ->
-            let newPos =
+            let adjacentSteps =
                 pos
                 |> adjacent
                 |> List.choose findPos
                 |> List.filter (fun (_, v) -> v = height + 1)
 
-            nextStep (newPos @ rest) acc
+            nextStep (adjacentSteps @ rest) acc
 
     nextStep [ startPos ] []
 
@@ -40,3 +37,10 @@ trailheads
 |> List.map (trailscore topoMap)
 |> List.map (List.countBy id >> List.length)
 |> List.sum
+|> printfn "Day 10a : %i"
+
+trailheads
+|> List.map (trailscore topoMap)
+|> List.map (List.length)
+|> List.sum
+|> printfn "Day 10b : %i"
