f2779ef1b83295e27c62c5f54fd2e95408d787e8
richard <rboss@kth.se>
2024-12-18 22:46:55 +0100

    day12

--- 2024/day12.fsx
+++ 2024/day12.fsx
@@ -1,9 +1,3 @@
-let example =
-    "./day12_example.txt" |> System.IO.File.ReadAllLines |> Array.map Seq.toArray
-
-let example2 =
-    "./day12_example2.txt" |> System.IO.File.ReadAllLines |> Array.map Seq.toArray
-
 let input =
     "./day12_input.txt" |> System.IO.File.ReadAllLines |> Array.map Seq.toArray
 
@@ -13,14 +7,33 @@ let gardenMap =
     |> Array.collect id
     |> Map.ofArray
 
+type FenceDirection =
+    | Top of int
+    | Right of int
+    | Bottom of int
+    | Left of int
+
+let sides map (y, x) =
+    [ Map.tryFind (y - 1, x) map |> Option.map (fun _ -> Top y, x)
+      Map.tryFind (y + 1, x) map |> Option.map (fun _ -> Bottom x, y + 1)
+      Map.tryFind (y, x - 1) map |> Option.map (fun _ -> Left x, y)
+      Map.tryFind (y, x + 1) map |> Option.map (fun _ -> Right(x + 1), y) ]
+    |> List.choose id
+
 let adjacent (y, x) =
     [ (y + 1, x); (y - 1, x); (y, x + 1); (y, x - 1) ]
 
-let findGroup map (initialPos, gType) =
+let plotNeighbours map gType pos =
     let findPos pos =
         Map.tryFind pos map
-        |> Option.filter (fun v -> v = gType)
+        |> Option.filter (fun c -> c = gType)
         |> Option.map (fun _ -> pos)
+
+    adjacent pos |> List.choose findPos
+
+let findGroup map (initialPos, gType) =
+    let findSides = sides map
+    let findPlotNeighbours = plotNeighbours map gType
 
     let rec nextPlot plots (positions, sides) =
         match plots with
@@ -29,16 +42,15 @@ let findGroup map (initialPos, gType) =
         | pos :: tail ->
             let newSet = Set.add pos positions
 
-            let adjacentPlots = pos |> adjacent |> List.choose findPos
-
-            let sidesCount = 4 - (List.length adjacentPlots)
+            let fences = findSides pos
+            let plotNeighbours = findPlotNeighbours pos
 
             let newAdjacent =
-                adjacentPlots |> List.filter (fun adjPos -> not (Set.contains adjPos newSet))
+                plotNeighbours |> List.filter (fun (adjPos) -> not (Set.contains adjPos newSet))
 
-            nextPlot (newAdjacent @ tail) (newSet, sides + sidesCount)
+            nextPlot (newAdjacent @ tail) (newSet, Set.union sides (fences |> Set.ofList))
 
-    nextPlot [ initialPos ] (Set.empty, 0)
+    nextPlot [ initialPos ] (Set.empty, Set.empty)
 
 
 let rec findNextGroup (gardenMap: Map<(int * int), char>) groups =
@@ -53,6 +65,26 @@ let rec findNextGroup (gardenMap: Map<(int * int), char>) groups =
 
     | None -> groups
 
+let countSequentialNum (numbers: int seq) : int =
+    numbers
+    |> Seq.sort
+    |> Seq.fold (fun (prev, acc) curr -> (curr, (if curr = prev + 1 then acc else acc + 1))) (System.Int32.MinValue, 0)
+    |> snd
+
+let calculateFenceCost (sides: Set<(FenceDirection * int)>) =
+    Set.toList sides
+    |> List.groupBy fst
+    |> List.map (fun (_, cords) -> cords |> List.map snd |> countSequentialNum)
+    |> List.sum
+
 findNextGroup gardenMap []
-|> List.map (fun (positions, sides) -> Set.count positions * sides)
+|> List.map (fun (positions, sides) -> Set.count positions * Set.count sides)
 |> List.sum
+|> printfn "Day 12a : %i"
+
+#time
+
+findNextGroup gardenMap []
+|> List.map (fun (positions, sides) -> Set.count positions * (calculateFenceCost sides))
+|> List.sum
+|> printfn "Day 12b : %i"
