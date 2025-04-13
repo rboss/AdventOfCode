d6c7c0ccefae81d385b086fb7f7c5caf647458d5
richard <rboss@kth.se>
2025-04-13 13:43:41 +0200

    day18 a

--- 2024/day18.fsx
+++ 2024/day18.fsx
@@ -1,9 +1,10 @@
 let input = "./input/day18_input.txt" |> System.IO.File.ReadAllLines
 let example = "./input/day18_example.txt" |> System.IO.File.ReadAllLines
 
-let validPos =
+let allValidPos =
     let obstacles =
-        example
+        input
+        |> Array.take 1024
         |> Array.map (fun row -> row.Split(","))
         |> Array.map (fun ([| X; Y |]) -> (int Y, int X))
 
@@ -13,8 +14,13 @@ let validPos =
 
     allPos |> List.toArray |> Array.except obstacles |> Set.ofArray
 
-let neighbours (Y, X) =
+let validPos pos = Set.contains pos allValidPos
+
+let adjacentPos (Y, X) =
     [ (Y + 1), X; (Y - 1), X; (Y), X + 1; (Y), X - 1 ]
+
+let neighbours pos =
+    pos |> adjacentPos |> List.filter validPos
 
 let tryDequeueLowest initState fCost priorityMap =
     let keepMinCost =
@@ -26,50 +32,44 @@ let tryDequeueLowest initState fCost priorityMap =
 
     let foldMinCost = Map.fold keepMinCost initState
 
-    if Map.count priorityMap = 0 then
+    if Map.isEmpty priorityMap then
         None
     else
         let (lowestKey, _lowestCost) = foldMinCost priorityMap
         Some((lowestKey, Map.find lowestKey priorityMap), Map.remove lowestKey priorityMap)
 
 let rec dijikstra map startPosition =
-    let tryDequeueLowestCost =
-        tryDequeueLowest (((0, 0)), (System.Int32.MaxValue, [])) (fun (cost, _) -> cost)
-
-    let isValidMapPos list =
-        list |> List.filter (fun ((pos), _) -> Set.contains pos map)
+    let tryDequeueLowestCost = tryDequeueLowest ((0, 0), System.Int32.MaxValue) id
 
     let notVisited visited list =
-        list |> List.filter (fun (v, _) -> Map.containsKey v visited |> not)
+        list |> List.filter (fun (pos) -> Map.containsKey pos visited |> not)
 
     let rec nextNode visitedMap queue =
         match tryDequeueLowestCost queue with
         | None -> visitedMap
         | Some(lowestQueue, newMap) ->
-            let ((pos), (cost, path)) = lowestQueue
+            let ((pos), (cost)) = lowestQueue
             let neighbours = neighbours pos
+            let neighbourCost = cost + 1
 
             let foldNeighbours =
-                fun queue (neighbourNode, neighbourCost) ->
+                fun queue (neighbourNode) ->
                     match Map.tryFind neighbourNode queue with
-                    | None -> Map.add neighbourNode (neighbourCost, (pos :: path)) queue
-                    | Some(queuedCost, queuedPath) ->
-                        if neighbourCost = queuedCost then
-                            Map.add neighbourNode (neighbourCost, path @ queuedPath) queue
-                        else if neighbourCost < queuedCost then
-                            Map.add neighbourNode (neighbourCost, pos :: path) queue
+                    | None -> Map.add neighbourNode neighbourCost queue
+                    | Some(queuedCost) ->
+                        if neighbourCost < queuedCost then
+                            Map.add neighbourNode neighbourCost queue
                         else
                             queue
 
             let newQueue =
-                neighbours
-                |> isValidMapPos
-                |> notVisited visitedMap
-                |> List.fold foldNeighbours newMap
+                neighbours |> notVisited visitedMap |> List.fold foldNeighbours newMap
 
-            let newVisited = Map.add (pos) (cost, path) visitedMap
+            let newVisited = Map.add (pos) (cost) visitedMap
 
             nextNode newVisited newQueue
 
-    let initialQueue = Map.empty |> Map.add startPosition (0, [])
+    let initialQueue = Map.empty |> Map.add startPosition 0
     nextNode Map.empty initialQueue
+
+dijikstra () (0, 0) |> Map.find (70, 70) |> printfn "Day 18a : %i"
