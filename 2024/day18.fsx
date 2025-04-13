678b9081bd2c703168ddbfbda71bb7322fe7b408
richard <rboss@kth.se>
2025-04-13 21:51:40 +0200

    day18b done

--- 2024/day18.fsx
+++ 2024/day18.fsx
@@ -1,26 +1,23 @@
 let input = "./input/day18_input.txt" |> System.IO.File.ReadAllLines
-let example = "./input/day18_example.txt" |> System.IO.File.ReadAllLines
 
-let allValidPos =
-    let obstacles =
-        input
-        |> Array.take 1024
-        |> Array.map (fun row -> row.Split(","))
-        |> Array.map (fun ([| X; Y |]) -> (int Y, int X))
+let allPos =
+    [ for y in 0..70 do
+          for x in 0..70 -> y, x ]
+    |> List.toArray
 
-    let allPos =
-        [ for y in 0..70 do
-              for x in 0..70 -> y, x ]
+let bytes =
+    input
+    |> Array.map (fun row -> row.Split(","))
+    |> Array.map (fun ([| X; Y |]) -> (int Y, int X))
 
-    allPos |> List.toArray |> Array.except obstacles |> Set.ofArray
-
-let validPos pos = Set.contains pos allValidPos
+let validPosAfterBytes byteNr =
+    allPos |> Array.except (Array.take byteNr bytes) |> Set.ofArray
 
 let adjacentPos (Y, X) =
     [ (Y + 1), X; (Y - 1), X; (Y), X + 1; (Y), X - 1 ]
 
-let neighbours pos =
-    pos |> adjacentPos |> List.filter validPos
+let neighbours validPos pos =
+    pos |> adjacentPos |> List.filter (fun pos -> Set.contains pos validPos)
 
 let tryDequeueLowest initState fCost priorityMap =
     let keepMinCost =
@@ -38,7 +35,7 @@ let tryDequeueLowest initState fCost priorityMap =
         let (lowestKey, _lowestCost) = foldMinCost priorityMap
         Some((lowestKey, Map.find lowestKey priorityMap), Map.remove lowestKey priorityMap)
 
-let rec dijikstra map startPosition =
+let rec dijikstra neighboursF startPosition goal =
     let tryDequeueLowestCost = tryDequeueLowest ((0, 0), System.Int32.MaxValue) id
 
     let notVisited visited list =
@@ -46,10 +43,10 @@ let rec dijikstra map startPosition =
 
     let rec nextNode visitedMap queue =
         match tryDequeueLowestCost queue with
-        | None -> visitedMap
+        | None -> None
+        | Some((pos, cost), _) when pos = goal -> Some(cost)
         | Some(lowestQueue, newMap) ->
             let ((pos), (cost)) = lowestQueue
-            let neighbours = neighbours pos
             let neighbourCost = cost + 1
 
             let foldNeighbours =
@@ -63,7 +60,7 @@ let rec dijikstra map startPosition =
                             queue
 
             let newQueue =
-                neighbours |> notVisited visitedMap |> List.fold foldNeighbours newMap
+                neighboursF pos |> notVisited visitedMap |> List.fold foldNeighbours newMap
 
             let newVisited = Map.add (pos) (cost) visitedMap
 
@@ -72,4 +69,31 @@ let rec dijikstra map startPosition =
     let initialQueue = Map.empty |> Map.add startPosition 0
     nextNode Map.empty initialQueue
 
-dijikstra () (0, 0) |> Map.find (70, 70) |> printfn "Day 18a : %i"
+let runForBytes bytes =
+    dijikstra (validPosAfterBytes bytes |> neighbours) (0, 0) (70, 70)
+
+runForBytes 1024 |> printfn "Day 18a : %A"
+
+let rec bisect f check initMin initMax =
+    let rec run min max =
+        let next = (min + max) / 2
+
+        if next = min then
+            next
+        else
+            match check (f next) with
+            | false -> run min next
+            | true -> run next max
+
+    run initMin initMax
+
+// brute force test
+seq [ 1024..4000 ]
+|> Seq.map (fun i -> i, runForBytes i)
+|> Seq.skipWhile (fun (_, res) -> Option.isSome res)
+|> Seq.take 1
+
+bisect runForBytes Option.isSome 1024 bytes.Length
+|> Array.get bytes
+|> fun (f, s) -> s, f
+|> printfn "Day 18b : %A"
