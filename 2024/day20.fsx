34bfd72eebbaa8f51de58979cf2c52c42960d10c
richard <rboss@kth.se>
2025-05-01 13:41:38 +0200

    day20 done

--- 2024/day20.fsx
+++ 2024/day20.fsx
@@ -5,13 +5,7 @@ let map =
     input
     |> Array.mapi (fun y row -> row |> Seq.toArray |> Array.mapi (fun x c -> (y, x), c))
     |> Array.collect id
-    // |> Array.filter (fun (_, c) -> c <> '#')
     |> Map.ofArray
-
-let height = map |> Map.keys |> Seq.maxBy fst
-let width = map |> Map.keys |> Seq.maxBy snd
-let startPos = Map.findKey (fun key c -> c = 'S') map
-let goalPos = Map.findKey (fun key c -> c = 'E') map
 
 let adjacentPos (Y, X) =
     [ (Y + 1), X; (Y - 1), X; (Y), X + 1; (Y), X - 1 ]
@@ -20,81 +14,48 @@ let neighbours map pos =
     pos
     |> adjacentPos
     |> List.choose (fun pos -> Map.tryFind pos map |> Option.map (fun v -> pos, v))
-// |> List.filter (fun (_, v) -> v <> '#')
 
+let calcDistances posMap =
+    let startPos = Map.findKey (fun _ c -> c = 'S') map
+    let goalPos = Map.findKey (fun _ c -> c = 'E') map
+    let neighboursF = neighbours posMap
 
-let tryDequeueLowest initState fCost priorityMap =
-    let keepMinCost =
-        (fun (minKey, minValue) key value ->
-            if fCost value < fCost minValue then
-                key, value
-            else
-                minKey, minValue)
+    let next (cost, prev, curr) =
+        if curr = startPos then
+            None
+        else
+            let nextCost = cost + 1
 
-    let foldMinCost = Map.fold keepMinCost initState
+            let nextPos =
+                neighboursF curr
+                |> List.filter (fun (pos, v) -> pos <> prev && v <> '#')
+                |> List.head
+                |> fst
 
-    if Map.isEmpty priorityMap then
-        None
-    else
-        let (lowestKey, _lowestCost) = foldMinCost priorityMap
-        Some((lowestKey, Map.find lowestKey priorityMap), Map.remove lowestKey priorityMap)
+            Some((nextPos, nextCost), (nextCost, curr, nextPos))
 
-let rec dijikstra neighboursF startPosition =
-    let tryDequeueLowestCost = tryDequeueLowest ((0, 0), System.Int32.MaxValue) id
+    (goalPos, 0) :: List.unfold next (0, (-1, -1), goalPos)
 
-    let notVisited visited list =
-        list |> List.filter (fun (pos) -> Map.containsKey pos visited |> not)
+let distanceArray = calcDistances map |> List.toArray |> Array.sortBy snd
 
-    let rec nextNode visitedMap queue =
-        match tryDequeueLowestCost queue with
-        | None -> visitedMap
-        | Some(lowestQueue, newMap) ->
-            let ((pos), (cost)) = lowestQueue
-            let neighbourCost = cost + 1
+let manhattanDistance (x2, y2) (x1, y1) = abs (y1 - y2) + abs (x1 - x2)
 
-            let foldNeighbours =
-                fun queue (neighbourNode) ->
-                    match Map.tryFind neighbourNode queue with
-                    | None -> Map.add neighbourNode neighbourCost queue
-                    | Some(queuedCost) ->
-                        if neighbourCost < queuedCost then
-                            Map.add neighbourNode neighbourCost queue
-                        else
-                            queue
+type DistanceArray = ((int * int) * int) array
 
-            let newQueue =
-                neighboursF pos |> notVisited visitedMap |> List.fold foldNeighbours newMap
+let shortCutsForPos (distances: DistanceArray) maxCheatDistance minSave =
+    fun index (startPos, startCost) ->
+        distances[(index + 1 + minSave) ..]
+        |> Seq.map (fun (pos, cost) -> pos, cost, manhattanDistance startPos pos)
+        |> Seq.filter (fun (_, _, cheatDistance) -> cheatDistance <= maxCheatDistance)
+        |> Seq.filter (fun (pos, cost, cheatDistance) -> cost - startCost - cheatDistance >= minSave)
 
-            let newVisited = Map.add (pos) (cost) visitedMap
+let findShortcuts (distances: DistanceArray) cheatDistance minSave =
+    let startDomain = distances[0 .. distances.Length - minSave]
 
-            nextNode newVisited newQueue
+    startDomain
+    |> Seq.mapi (shortCutsForPos distances cheatDistance minSave)
+    |> Seq.collect id
 
-    let initialQueue = Map.empty |> Map.add startPosition 0
-    nextNode Map.empty initialQueue
+findShortcuts distanceArray 2 100 |> Seq.length |> sprintf "Day 20a : %i"
 
-let neighboursF = neighbours map >> List.filter (fun (_, v) -> v <> '#')
-let distances = dijikstra (neighboursF >> List.map fst) goalPos
-
-
-let findShortcuts map startPos =
-    neighbours map startPos
-    |> List.filter (fun (_, v) -> v = '#')
-    |> List.map fst
-    |> List.collect (neighbours map)
-    |> List.filter (fun (_, v) -> v = '.' || v = 'E')
-    |> List.filter (fun (endPos, _) -> endPos <> startPos)
-    |> List.map (fun (endPos, _) -> (startPos, endPos))
-
-let calculateSavings distances (startPos, endPos) =
-    let startCost = Map.find startPos distances
-    let endCost = Map.find endPos distances
-    -1 * (endCost - startCost + 2)
-
-let shortcutStartPos =
-    map |> Map.filter (fun _ c -> c = '.' || c = 'S') |> Map.toList |> List.map fst
-
-shortcutStartPos
-|> List.collect (findShortcuts map)
-|> List.map (calculateSavings distances)
-|> List.filter ((<=) 100)
-|> List.length
+findShortcuts distanceArray 20 100 |> Seq.length |> sprintf "Day 20b : %i"
