469ea9eab0a24c8e7014aff6866b1124ca7c1a89
richard <rboss@kth.se>
2025-05-09 20:49:51 +0200

    22b

--- 2024/day22.fsx
+++ 2024/day22.fsx
@@ -1,5 +1,11 @@
 let example = "./input/day22_example.txt" |> System.IO.File.ReadAllLines
+let example2 = "./input/day22_example2.txt" |> System.IO.File.ReadAllLines
 let input = "./input/day22_input.txt" |> System.IO.File.ReadAllLines
+
+type PriceHistory =
+    { Secret: uint64
+      Price: int
+      ChangeHistory: int list }
 
 let process f secret =
     secret |> f |> (fun m -> secret ^^^ m) |> (fun m -> m % 16777216UL)
@@ -12,19 +18,48 @@ let nextSecret =
     p1 >> p2 >> p3
 
 let secretGenerator secret =
-    Seq.unfold (fun sec -> Some(nextSecret sec, nextSecret sec)) secret
+    Seq.unfold
+        (fun
+            { Secret = prevSecret
+              Price = prevPrice
+              ChangeHistory = [ h1; h2; h3; _ ] } ->
+            let newSec = nextSecret prevSecret
+            let newPrice = newSec % 10UL |> int
+
+            let newHistory =
+                { Secret = newSec
+                  Price = newPrice
+                  ChangeHistory = (newPrice - prevPrice) :: [ h1; h2; h3 ] }
+
+            Some(newHistory, newHistory))
+        { Secret = secret
+          Price = 3
+          ChangeHistory = [ 0; 0; 0; 0 ] }
 
 nextSecret 123UL |> nextSecret
 nextSecret 123UL |> nextSecret |> nextSecret
 nextSecret 16495136UL
-secretGenerator 123UL |> Seq.skip 9 |> Seq.take 1
+secretGenerator 123UL |> Seq.skip 3 |> Seq.take 6 |> Seq.toList
 
 example
 |> Seq.map (uint64)
 |> Seq.map (secretGenerator >> Seq.skip 1999 >> Seq.head)
 
-
 input
 |> Seq.map (uint64)
 |> Seq.map (secretGenerator >> Seq.skip 1999 >> Seq.head)
+|> Seq.map (_.Secret)
 |> Seq.sum
+|> sprintf "Day 22a : %i"
+
+
+input
+|> Seq.map (uint64)
+|> Seq.map (secretGenerator >> Seq.skip 3 >> Seq.take 1996)
+|> Seq.map (fun s -> s |> Seq.groupBy _.ChangeHistory |> Seq.map (snd >> Seq.head))
+|> Seq.collect id
+|> Seq.groupBy (_.ChangeHistory)
+|> Seq.maxBy (fun (_, historySeq) -> historySeq |> Seq.sumBy ( _.Price ))
+|> (fun (_, historySeq) -> historySeq |> Seq.sumBy ( _.Price ))
+|> sprintf "Day 22b : %i"
+
