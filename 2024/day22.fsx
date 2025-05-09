13ada1a0973eb636bfde5fa0a83053fdf478a938
richard <rboss@kth.se>
2025-05-09 22:53:13 +0200

    day22 opti refac

--- 2024/day22.fsx
+++ 2024/day22.fsx
@@ -1,19 +1,18 @@
-let example = "./input/day22_example.txt" |> System.IO.File.ReadAllLines
-let example2 = "./input/day22_example2.txt" |> System.IO.File.ReadAllLines
 let input = "./input/day22_input.txt" |> System.IO.File.ReadAllLines
+let example2 = "./input/day22_example2.txt" |> System.IO.File.ReadAllLines
 
 type PriceHistory =
     { Secret: uint64
       Price: int
-      ChangeHistory: int list }
+      History: int; }
 
 let process f secret =
-    secret |> f |> (fun m -> secret ^^^ m) |> (fun m -> m % 16777216UL)
+    ((f secret) ^^^ secret) % 16777216UL
 
 let nextSecret =
-    let p1 = process ((*) 64UL)
-    let p2 = process (fun m -> m / 32UL)
-    let p3 = process ((*) 2048UL)
+    let p1 = process (fun s -> s <<< 6)
+    let p2 = process (fun s -> s >>> 5)
+    let p3 = process (fun s -> s <<< 11)
 
     p1 >> p2 >> p3
 
@@ -22,28 +21,20 @@ let secretGenerator secret =
         (fun
             { Secret = prevSecret
               Price = prevPrice
-              ChangeHistory = [ h1; h2; h3; _ ] } ->
+              History = prevHistory } ->
             let newSec = nextSecret prevSecret
             let newPrice = newSec % 10UL |> int
+            let newHistory = (prevHistory <<< 5) ||| (newPrice + 9 - prevPrice) &&& 0xFFFFF
 
             let newHistory =
                 { Secret = newSec
                   Price = newPrice
-                  ChangeHistory = (newPrice - prevPrice) :: [ h1; h2; h3 ] }
+                  History = newHistory }
 
             Some(newHistory, newHistory))
         { Secret = secret
           Price = 3
-          ChangeHistory = [ 0; 0; 0; 0 ] }
-
-nextSecret 123UL |> nextSecret
-nextSecret 123UL |> nextSecret |> nextSecret
-nextSecret 16495136UL
-secretGenerator 123UL |> Seq.skip 3 |> Seq.take 6 |> Seq.toList
-
-example
-|> Seq.map (uint64)
-|> Seq.map (secretGenerator >> Seq.skip 1999 >> Seq.head)
+          History = 0 }
 
 input
 |> Seq.map (uint64)
@@ -52,14 +43,12 @@ input
 |> Seq.sum
 |> sprintf "Day 22a : %i"
 
-
 input
 |> Seq.map (uint64)
 |> Seq.map (secretGenerator >> Seq.skip 3 >> Seq.take 1996)
-|> Seq.map (fun s -> s |> Seq.groupBy _.ChangeHistory |> Seq.map (snd >> Seq.head))
+|> Seq.map (Seq.groupBy _.History >> Seq.map (snd >> Seq.head))
 |> Seq.collect id
-|> Seq.groupBy (_.ChangeHistory)
-|> Seq.maxBy (fun (_, historySeq) -> historySeq |> Seq.sumBy ( _.Price ))
-|> (fun (_, historySeq) -> historySeq |> Seq.sumBy ( _.Price ))
+|> Seq.groupBy (_.History)
+|> Seq.maxBy (snd >> Seq.sumBy ( _.Price ))
+|> (snd >> Seq.sumBy ( _.Price ))
 |> sprintf "Day 22b : %i"
-
