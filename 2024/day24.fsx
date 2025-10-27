64b25c96f69f80419d1cc566e534c8c1f1785916
Richard Boss <richard.boss@funnel.io>
2025-10-27 09:41:01 +0100

    day24ongoing

--- 2024/day24.fsx
+++ 2024/day24.fsx
@@ -1,4 +1,3 @@
-
 #load "Shared.fsx"
 open Shared
 open System
@@ -11,6 +10,8 @@ type GateInput =
     | Xor of string * string
     | Value of int
 
+let sortIn (a, b) = if a < b then (a, b) else (b, a)
+
 let parseInput (input : string) = 
   let [|inputs; gates|] = input.Split(Environment.NewLine + Environment.NewLine)
 
@@ -22,9 +23,9 @@ let parseInput (input : string) =
     gates.Split(Environment.NewLine)
     |> Array.map (fun line -> 
         match line with
-        | Regex @"(\w+) AND (\w+) -> (\w+)" [in1; in2; out] -> out, And (in1, in2)
-        | Regex @"(\w+) OR (\w+) -> (\w+)" [in1; in2; out] -> out, Or (in1, in2)
-        | Regex @"(\w+) XOR (\w+) -> (\w+)" [in1; in2; out] -> out, Xor (in1, in2)
+        | Regex @"(\w+) AND (\w+) -> (\w+)" [in1; in2; out] -> out, And (sortIn(in1, in2))
+        | Regex @"(\w+) OR (\w+) -> (\w+)" [in1; in2; out] -> out, Or (sortIn(in1, in2))
+        | Regex @"(\w+) XOR (\w+) -> (\w+)" [in1; in2; out] -> out, Xor (sortIn(in1, in2))
     )
 
   Array.concat [valueInputs; gatesInputs] |> Map.ofArray
@@ -62,3 +63,80 @@ let solve1 input =
 
 solve1 input
 |> printfn "Day 24a : %i"
+
+let revTuple (x, y) = (y, x)
+
+let revGateMap = 
+  Map.toList (parseInput input) 
+  |> List.map revTuple 
+  |> Map.ofList
+
+
+let fullAdder map cIn a b =
+  let find x = Map.find x map
+  let xorAB = find(Xor (sortIn (a, b)))
+  let andAB = find(And (sortIn (a, b)))
+  let cInXor = find(And (sortIn (xorAB, cIn)))
+  let sum = find(Xor (sortIn (xorAB, cIn)))
+  let cout = find(Or (sortIn (cInXor, andAB)))
+
+  (sum, cout)
+
+let halfAdder map a b =
+  let find x = Map.find x map
+  let sum = find(Xor (sortIn (a, b)))
+  let cout = find(And (sortIn (a, b)))
+
+  (sum, cout)
+
+let (_, cout0) = halfAdder revGateMap "x00" "y00"
+
+[1..44]
+|> List.fold 
+  (fun cin i -> 
+    printfn "finding full adder for bit %d - %A" i cin
+    let (zOut, cout) = fullAdder revGateMap cin (sprintf "x%02d" i) (sprintf "y%02d" i)
+    printfn "found: zOut=%s cout=%A" zOut cout
+    cout
+  ) 
+  cout0
+
+
+let fullAdderOutputs map cIn a b =
+  let find x = 
+    printfn "Finding gate %A" x
+    Map.find x map
+
+  let xorAB = find(Xor (sortIn (a, b)))
+  let andAB = find(And (sortIn (a, b)))
+
+  let cInOuts = 
+    Map.filter (fun k v -> 
+      match k with 
+      | And (i1, i2) when i1 = cIn || i2 = cIn -> true 
+      | Xor (i1, i2) when i1 = cIn || i2 = cIn -> true 
+      | _ -> false ) map
+    |> Map.values
+
+  let abXorAndOuts = 
+    Map.filter (fun k v -> 
+      match k with 
+      | Or (i1, i2) when i1 = xorAB || i2 = xorAB -> true 
+      | Or (i1, i2) when i1 = andAB || i2 = andAB -> true 
+      | Xor (i1, i2) when i1 = xorAB || i2 = xorAB -> true 
+      | Xor (i1, i2) when i1 = andAB || i2 = andAB -> true 
+      | _ -> false ) map
+    |> Map.values
+
+  Seq.concat [seq[andAB]; seq[xorAB]; cInOuts; abXorAndOuts] |> Seq.toList
+
+fullAdderOutputs revGateMap "kcm" "x16" "y16"
+
+
+let cIn = "kcm"
+Map.filter (fun k v -> 
+  match k with 
+  | And (i1, i2) when i1 = cIn || i2 = cIn -> true 
+  | Xor (i1, i2) when i1 = cIn || i2 = cIn -> true 
+  | _ -> false ) revGateMap
+|> Map.values
