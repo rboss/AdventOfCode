e3ce61ab0ccb65858992ac2b08dd73821f2e5575
Richard Boss <richard.boss@funnel.io>
2025-10-28 21:49:09 +0100

    minor refac day24b

--- 2024/day24.fsx
+++ 2024/day24.fsx
@@ -1,7 +1,6 @@
 #load "Shared.fsx"
 open Shared
 open System
-let example = "./input/day24_example2.txt" |> IO.File.ReadAllText
 let input = "./input/day24_input.txt" |> IO.File.ReadAllText
 
 type GateInput =
@@ -62,14 +61,19 @@ let solve1 input =
 solve1 input
 |> printfn "Day 24a : %i"
 
+let tryFindGateSorted map = function
+  | And (i1, i2) -> Map.tryFind (And (sortInput (i1, i2))) map
+  | Or (i1, i2) -> Map.tryFind (Or (sortInput (i1, i2))) map
+  | Xor (i1, i2) -> Map.tryFind (Xor (sortInput (i1, i2))) map
+  | Value x -> Map.tryFind (Value x) map
 
 let checkFullAdder gateMap cIn a b = maybe {
-  let tryFind x = Map.tryFind x gateMap
-  let! xorAB = tryFind(Xor (sortInput (a, b)))
-  let! andAB = tryFind(And (sortInput (a, b)))
-  let! cInXor = tryFind(And (sortInput (xorAB, cIn)))
-  let! sum = tryFind(Xor (sortInput (xorAB, cIn)))
-  let! cout = tryFind(Or (sortInput (cInXor, andAB)))
+  let tryFind = tryFindGateSorted gateMap
+  let! xorAB = tryFind(Xor (a, b))
+  let! andAB = tryFind(And (a, b))
+  let! cInXor = tryFind(And (xorAB, cIn))
+  let! sum = tryFind(Xor (xorAB, cIn))
+  let! cout = tryFind(Or (cInXor, andAB))
   
   if not (sum.StartsWith "z") then
     return! None // sum must be a z register
@@ -84,10 +88,8 @@ let halfAdder gateMap a b =
 
   (sum, cout)
 
-let fullAdderOutputs map cIn a b =
-  let find x = 
-    printfn "Finding gate %A" x
-    Map.find x map
+let adderPossibleOutputs map cIn a b =
+  let find x = Map.find x map
 
   let xorAB = find(Xor (sortInput (a, b)))
   let andAB = find(And (sortInput (a, b)))
@@ -98,7 +100,7 @@ let fullAdderOutputs map cIn a b =
       | And (i1, i2) when i1 = cIn || i2 = cIn -> true 
       | Xor (i1, i2) when i1 = cIn || i2 = cIn -> true 
       | _ -> false ) map
-    |> Map.values
+    |> Map.values |> Seq.toList
 
   let abXorAndOuts = 
     Map.filter (fun k v -> 
@@ -108,49 +110,47 @@ let fullAdderOutputs map cIn a b =
       | Xor (i1, i2) when i1 = xorAB || i2 = xorAB -> true 
       | Xor (i1, i2) when i1 = andAB || i2 = andAB -> true 
       | _ -> false ) map
-    |> Map.values
+    |> Map.values |> Seq.toList
 
-  Seq.concat [seq[andAB]; seq[xorAB]; cInOuts; abXorAndOuts] |> Seq.toList
+  xorAB :: andAB :: cInOuts @ abXorAndOuts
 
 let allPairsSelf list = 
   List.allPairs list list |> List.filter (fun (x, y) -> x <> y)
 
 let invertMap map = map |> Map.toList |> List.map (fun (k, v) -> v, k) |> Map.ofList
 
+let swapMapValues map v1 v2 = 
+  let v1Key = Map.findKey (fun k v -> v = v1) map
+  let v2Key = Map.findKey (fun k v -> v = v2) map
+
+  map 
+  |> Map.change (v1Key) (fun _ -> Some v2) 
+  |> Map.change (v2Key) (fun _ -> Some v1) 
+
 let solve2 input =
   let gateMap = parseInput input 
   let maxZ = maxZgate gateMap
   let revGateMap = invertMap gateMap
 
-  let (sum, out0) = halfAdder revGateMap "x00" "y00"
-  [1..(maxZ - 1)] |> List.mapFold 
-    (fun cin i -> 
+  let gateForPos cin i = 
       let xInput = sprintf "x%02d" i
       let yInput = sprintf "y%02d" i
       let out = checkFullAdder revGateMap cin xInput yInput
       match out with 
-      | Some (zOut, cout) -> 
-        (None, cout)
+      | Some (_, cout) -> [], cout
       | None -> 
-        let outGates = fullAdderOutputs revGateMap cin xInput yInput
+        let outGates = adderPossibleOutputs revGateMap cin xInput yInput
         let possibleSwapGates = allPairsSelf outGates
-        let ((sum, cout), swapGates) = 
-          possibleSwapGates |> List.pick (fun (g1, g2) -> 
-            printfn "Trying swap gates %s and %s" g1 g2
-            let tryMap = 
-              revGateMap
-              |> Map.map (fun k v -> if v = g1 then "_tmp_" else v)
-              |> Map.map (fun k v -> if v = g2 then g1 else v)
-              |> Map.map (fun k v -> if v = "_tmp_" then g2 else v)
+        possibleSwapGates |> List.pick (fun (g1, g2) -> 
+          let swappedMap =  swapMapValues revGateMap g1 g2
+          checkFullAdder swappedMap cin xInput yInput |> Option.map (fun (_, cout) -> [g1; g2], cout))
 
-            checkFullAdder tryMap cin xInput yInput
-            |> Option.map (fun res -> 
-              res, (g1,g2) ))
-        (Some swapGates, cout)
-    ) out0
+  let (_, cout0) = halfAdder revGateMap "x00" "y00"
+
+  [1..(maxZ - 1)] 
+  |> List.mapFold gateForPos cout0
   |> fst
-  |> List.choose id
-  |> List.collect (fun (g1, g2) -> [g1; g2])
+  |> List.collect id
   |> List.sort
   |> String.concat ","
 
